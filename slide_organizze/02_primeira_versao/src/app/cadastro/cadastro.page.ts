f27606bd6a8686f0b4833d11c0a36dd3fdffa012
Jeferson Ferreira <jefender@gmail.com>
2021-03-11 20:06:25 -0300

    Add nova versão do Organizza em Side Menu

---
+++ slide_organizze/02_primeira_versao/src/app/cadastro/cadastro.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.page.html',
+  styleUrls: ['./cadastro.page.scss'],
+})
+export class CadastroPage implements OnInit {
+
+  constructor(public navCtrl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  carregarTela(tela) {
+    this.navCtrl.navigateForward(tela);
+  }
+
+}
